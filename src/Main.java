f82121a11335bef3ed76ec598d4cd618586d08c1
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-10-27 21:02:02 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Sıralama öncesi:");
+        for (int num : arr) System.out.println(num + " ");
 
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
 
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
         }
+
+        System.out.println("\n\n Sıralama sonrası:");
+        for (int num : arr) System.out.println(num + " ");
+    }
 }
